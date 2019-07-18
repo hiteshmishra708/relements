feb3b91c6f5e31d4a3c394441d23554bd5cb2127
Kartik Grewal <kartik.grewal@haptik.co>
2019-07-18 15:34:10 +0530

    test(Loader): Added unit tests, props and documentation (#7)
    * test(Loader): Added unit tests, props and documentation
    
    Added unit tests, props and documentation
    
    * Just a test file I created way back to check
    
    * Updated Loader css
    
    * style(Loader): Changed prefix root
    
    changed prefix and root
    
    * Single quotes

--- src/components/UI/Icon/__tests__/Icon.test.js
+++ src/components/UI/Icon/__tests__/Icon.test.js
@@ -1,27 +1,26 @@
 /* eslint-env jest */
 
-import React from 'react';
-import 'jest-dom/extend-expect';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import React from "react";
+import "jest-dom/extend-expect";
+import { render, cleanup } from "@testing-library/react";
 
-import { ICON_MAP } from '../utils/getIcon';
-import Icon from '../Icon';
+import { ICON_MAP } from "../utils/getIcon";
+import Icon from "../Icon";
 
 afterEach(cleanup);
 
-test('Smoke', async () => {
+test("Smoke", async () => {
   const { queryAllByTestId, rerender, container } = render(<Icon />);
-  expect(queryAllByTestId('icon').length).toBe(0)
+  expect(queryAllByTestId("icon").length).toBe(0);
 
   Object.keys(ICON_MAP).forEach(key => {
-    rerender(<Icon src={key}/>);
-    expect(queryAllByTestId('icon').length).toBe(1)
-    expect(container.getElementsByTagName('svg').length).toBe(1)
-  })
+    rerender(<Icon src={key} />);
+    expect(queryAllByTestId("icon").length).toBe(1);
+    expect(container.getElementsByTagName("svg").length).toBe(1);
+  });
 });
 
-test('Custom class', async () => {
-  const { getByTestId } = render(<Icon src="info" className="test"/>);
-  expect(getByTestId('icon')).toHaveClass('test');
+test("Custom class", async () => {
+  const { getByTestId } = render(<Icon src="info" className="test" />);
+  expect(getByTestId("icon")).toHaveClass("test");
 });
-
