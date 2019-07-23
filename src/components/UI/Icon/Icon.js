f619b5d8ed4ff74ed6273e0f791a5a677646a270
Shivang Bhandari <pump382156@gmail.com>
2019-07-23 15:32:33 +0530

    [Docs + Tests] UI-Icon #PNG-3153

--- src/components/UI/Icon/Icon.js
+++ src/components/UI/Icon/Icon.js
@@ -3,34 +3,88 @@ import PropTypes from 'prop-types';
 
 import styles from './Icon.scss';
 import { getIcon } from './utils/getIcon';
+import WithTooltip from '../../Overlays/WithTooltip';
 
 /**
  * Icon component that renders an icon. It takes a string identifier
  * and renders the icon corresponding to that.
  */
 const Icon = ({
-  src = '', className = '', size, onClick, innerRef,
+  src = '', prefixClassName = '', className = '', size, onClick, innerRef, tooltip, tooltipPosition,
 }) => {
   if (!src) return null;
+
+  const getSizeClassName = () => {
+    switch (size) {
+      case Icon.SIZES.BIG:
+        return styles.big;
+      case Icon.SIZES.MEDIUM:
+        return styles.medium;
+      case Icon.SIZES.SMALL:
+        return styles.small;
+      default:
+        return styles.medium;
+    }
+  };
+
   const IconSvg = typeof src === 'string' ? getIcon(src) : src;
+
   return (
-    <div data-testid="icon" ref={innerRef} onClick={onClick} className={`${styles.icon} ${className}`}>
-      <IconSvg />
+    <div
+      data-testid="icon"
+      ref={innerRef}
+      onClick={onClick}
+      className={`${styles.icon} ${prefixClassName} ${className} ${getSizeClassName()}`}
+    >
+      <WithTooltip
+        tooltip={tooltip}
+        tooltipPosition={tooltipPosition}
+        prefixClassName={`${prefixClassName}-tooltip`}
+      >
+        <IconSvg className={`${prefixClassName}-svg`} />
+      </WithTooltip>
     </div>
   );
 };
 
+Icon.SIZES = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  BIG: 'big',
+};
+
+Icon.defaultProps = {
+  src: '',
+  className: '',
+  tooltip: '',
+  tooltipPosition: '',
+  size: Icon.SIZES.MEDIUM,
+  onClick: () => {},
+};
+
 Icon.propTypes = {
-  /** Can either be a string identifier for the icon (angle-down) or an react node */
+  /** Can either be a string identifier for the icon (angle-down) or a react node */
   src: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  /** The classname to be attached to the icons outer most div */
+  /** The classname to be prefixed to the icon's outer most div */
+  prefixClassName: PropTypes.string,
+  /** The classname to be attached to the icon's outer most div */
   className: PropTypes.string,
-  /** The size of the icon (small/medium/large/px number)  */
+  /** The size of the icon (small/medium/big/px number)  */
   size: PropTypes.number,
   /** The callback function that will be called on click of the icon */
   onClick: PropTypes.func,
   /** Passes the ref to the outermost div  */
   innerRef: PropTypes.func,
+  /** Tooltip for the Icon */
+  tooltip: PropTypes.string,
+  /** Tooltip Positioning (TOP/BOTTOM) */
+  tooltipPosition: PropTypes.string,
+};
+
+Icon.classNames = {
+  $prefix: 'Outermost element',
+  '$prefix-tooltip': 'Tooltip wrapping the Icon',
+  '$prefix-svg': 'The SVG wrapped in icon element',
 };
 
 export default Icon;
--- src/components/UI/Icon/__tests__/Icon.test.js
+++ src/components/UI/Icon/__tests__/Icon.test.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "jest-dom/extend-expect";
-import { render, cleanup } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 
 import { ICON_MAP } from "../utils/getIcon";
 import Icon from "../Icon";
@@ -20,7 +20,51 @@ test("Smoke", async () => {
   });
 });
 
+test("Size variations", async () => {
+  const { getByTestId, rerender } = render(<Icon />);
+  Object.keys(Icon.SIZES).forEach(type => {
+    rerender(<Icon src="info" size={Icon.SIZES[type]} />);
+    expect(getByTestId("icon")).toBeInTheDocument();
+  });
+
+  // invalid size
+  rerender(<Icon src="info" size="123" />);
+  expect(getByTestId("icon")).toBeInTheDocument();
+});
+
 test("Custom class", async () => {
   const { getByTestId } = render(<Icon src="info" className="test" />);
   expect(getByTestId("icon")).toHaveClass("test");
+});
+
+test("Prefix Class", async () => {
+  const classNames = Object.keys(Icon.classNames).map(className =>
+    className.replace("$prefix", "test"),
+  );
+  // this pops out the className 'test-svg' (svgs are mocked so can't test that)
+  classNames.pop();
+  const { container } = render(
+    <Icon src="info" tooltip="this is a demo tooltip" prefixClassName="test" />,
+  );
+
+  classNames.forEach(className => {
+    expect(
+      document.getElementsByClassName(className).length,
+      className,
+    ).toBeGreaterThanOrEqual(1);
+  });
+});
+
+test("Click", async () => {
+  const mockOnClick = jest.fn();
+  const { getByTestId } = render(<Icon src="info" onClick={mockOnClick} />);
+  fireEvent.click(getByTestId("icon"));
+  expect(mockOnClick).toHaveBeenCalledTimes(1);
+});
+
+test("Ref", async () => {
+  const mockRef = jest.fn();
+  const { getByTestId } = render(<Icon src="info" innerRef={mockRef} />);
+  fireEvent.click(getByTestId("icon"));
+  expect(mockRef).toHaveBeenCalledTimes(1);
 });
