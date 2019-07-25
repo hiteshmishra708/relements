b51bcc378b322a3b25866af70374aebf00152f16
Kartik Grewal <kartik.grewal@haptik.co>
2019-07-25 18:52:39 +0530

    refactor(Toggle): Docs and tests for Toggle #PE-604 (#14)
    * refactor(Toggle): Docs and tests for Toggle
    
    Docs and tests for Toggle
    
    * test(Toggle): Change the test and added static classnames property
    
    Change the test and added static classnames property

--- src/components/Inputs/Toggle/Toggle.storybook.js
+++ src/components/Inputs/Toggle/Toggle.storybook.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import Toggle from "./Toggle";
+import Docs from "./Toggle.mdx";
 
-const VALUE = "";
-
-storiesOf("Components|Inputs/Toggle", module).add("Single", () => {
-  const story = (
-    <div>
-      <Toggle label="Toggle Input" value={VALUE} onChange={console.log} />
-    </div>
-  );
-  return story;
-});
+storiesOf("Components|Inputs/Toggle", module).add("Documentation", () => (
+  <Docs />
+));
