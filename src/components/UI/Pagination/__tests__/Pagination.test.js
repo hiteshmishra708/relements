6725726793252a96abc80def9a3474b744f7e019
Kartik Grewal <kartik.grewal@haptik.co>
2019-07-31 14:19:11 +0530

    refactor(Pagination): Docs, example and tests for pagination (#21)
    Docs, examples and tests for pagination

--- src/components/UI/Pagination/__tests__/Pagination.test.js
+++ src/components/UI/Pagination/__tests__/Pagination.test.js
@@ -1,58 +1,119 @@
 /* eslint-env jest */
 
-import React from 'react';
-import 'jest-dom/extend-expect';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import React from "react";
+import "jest-dom/extend-expect";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 
-import Pagination from '../Pagination';
+import Pagination from "../Pagination";
 
 afterEach(cleanup);
 
-test('Smoke', async () => {
-  const { queryAllByTestId, rerender } = render(<Pagination numPages={33} page={1} />);
-  expect(queryAllByTestId('pagination').length).toBe(1)
+test("Smoke", async () => {
+  const { queryAllByTestId } = render(<Pagination numPages={33} page={1} />);
+  expect(queryAllByTestId("pagination").length).toBe(1);
 });
 
-test('Custom class', async () => {
-  const { getByTestId } = render(<Pagination numPages={33} page={1} className="test"/>);
-  expect(getByTestId('pagination')).toHaveClass('test');
+test("Custom class", async () => {
+  const { getByTestId } = render(
+    <Pagination numPages={33} page={1} className="test" />,
+  );
+  expect(getByTestId("pagination")).toHaveClass("test");
 });
 
-test('Next page', async () => {
+test("prefixClassName test", async () => {
+  const classNames = Object.keys(Pagination.classNames).map(className =>
+    className.replace("$prefix", "test"),
+  );
+
+  const { getByTestId } = render(<Pagination prefixClassName="test" />);
+
+  classNames.forEach(className => {
+    expect(
+      document.getElementsByClassName(className).length,
+      className,
+    ).toBeGreaterThanOrEqual(1);
+  });
+});
+
+test("Next page", async () => {
   const mockFn = jest.fn();
-  const { getByTestId, container, rerender } = render(<Pagination numPages={33} page={1} prefixClassName="test" onChange={mockFn}/>);
-  const nextPageButton = container.getElementsByClassName('test-arrow-right')[0];
-  
+  const { container, rerender } = render(
+    <Pagination
+      numPages={33}
+      page={1}
+      prefixClassName="test"
+      onChange={mockFn}
+    />,
+  );
+  const nextPageButton = container.getElementsByClassName(
+    "test-arrow-right",
+  )[0];
+
   fireEvent.click(nextPageButton);
   expect(mockFn).toHaveBeenCalledTimes(1);
   expect(mockFn.mock.calls[0][0]).toBe(2);
-  rerender(<Pagination numPages={33} page={2} prefixClassName="test" onChange={mockFn}/>)
-  const activePage = container.getElementsByClassName('test-page-active')[0];
-  expect(activePage).toHaveTextContent('2');
-})
+  rerender(
+    <Pagination
+      numPages={33}
+      page={2}
+      prefixClassName="test"
+      onChange={mockFn}
+    />,
+  );
+  const activePage = container.getElementsByClassName("test-page-active")[0];
+  expect(activePage).toHaveTextContent("2");
+});
 
-test('Previous page', async () => {
+test("Previous page", async () => {
   const mockFn = jest.fn();
-  const { getByTestId, container, rerender } = render(<Pagination numPages={33} page={2} prefixClassName="test" onChange={mockFn}/>);
-  const nextPageButton = container.getElementsByClassName('test-arrow-left')[0];
-  
+  const { container, rerender } = render(
+    <Pagination
+      numPages={33}
+      page={2}
+      prefixClassName="test"
+      onChange={mockFn}
+    />,
+  );
+  const nextPageButton = container.getElementsByClassName("test-arrow-left")[0];
+
   fireEvent.click(nextPageButton);
   expect(mockFn).toHaveBeenCalledTimes(1);
   expect(mockFn.mock.calls[0][0]).toBe(1);
-  rerender(<Pagination numPages={33} page={1} prefixClassName="test" onChange={mockFn}/>)
-  const activePage = container.getElementsByClassName('test-page-active')[0];
-  expect(activePage).toHaveTextContent('1');
-})
+  rerender(
+    <Pagination
+      numPages={33}
+      page={1}
+      prefixClassName="test"
+      onChange={mockFn}
+    />,
+  );
+  const activePage = container.getElementsByClassName("test-page-active")[0];
+  expect(activePage).toHaveTextContent("1");
+});
 
-test('Specific page', async () => {
+test("Specific page", async () => {
   const mockFn = jest.fn();
-  const { getByTestId, container, rerender } = render(<Pagination numPages={4} page={2} prefixClassName="test" onChange={mockFn}/>);
-  const pages = container.getElementsByClassName('test-page');
-  
+  const { container, rerender } = render(
+    <Pagination
+      numPages={4}
+      page={2}
+      prefixClassName="test"
+      onChange={mockFn}
+    />,
+  );
+  const pages = container.getElementsByClassName("test-page");
+
   fireEvent.click(pages[3]);
   expect(mockFn).toHaveBeenCalledTimes(1);
   expect(mockFn.mock.calls[0][0]).toBe(4);
-  rerender(<Pagination numPages={4} page={4} prefixClassName="test" onChange={mockFn}/>)
-  const activePage = container.getElementsByClassName('test-page-active')[0];
-  expect(activePage).toHaveTextContent('4');
-})
+  rerender(
+    <Pagination
+      numPages={4}
+      page={4}
+      prefixClassName="test"
+      onChange={mockFn}
+    />,
+  );
+  const activePage = container.getElementsByClassName("test-page-active")[0];
+  expect(activePage).toHaveTextContent("4");
+});
