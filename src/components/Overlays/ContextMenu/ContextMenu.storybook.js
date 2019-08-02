6f9765e9c42360501c2e6a207f9917e04f6d7e3c
Pratham Agrawal <pratham.agrawal@haptik.co>
2019-08-02 17:44:25 +0530

    feat(contextmenu): docs + tests (#24)
    * docs(contextmenu): documentation
    * test(contextmenu): unit tests

--- src/components/Overlays/ContextMenu/ContextMenu.storybook.js
+++ src/components/Overlays/ContextMenu/ContextMenu.storybook.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import Docs from "./ContextMenu.mdx";
+import Docs from "./docs/ContextMenu.mdx";
+import ContextMenuButtonDocs from "./docs/ContextMenu.Button.mdx";
+import ContextMenuIconDocs from "./docs/ContextMenu.Icon.mdx";
+import ContextMenuItemDocs from "./docs/ContextMenu.Item.mdx";
 
 // eslint-disable-next-line
-storiesOf("Components|Overlays/ContextMenu", module).add(
-  "Documentation",
-  () => <Docs />,
-);
+storiesOf("Components|Overlays/ContextMenu", module)
+  .add("Documentation", () => <Docs />)
+  .add("ContextMenu.Icon", () => <ContextMenuIconDocs />)
+  .add("ContextMenu.Button", () => <ContextMenuButtonDocs />)
+  .add("ContextMenu.Item", () => <ContextMenuItemDocs />);
--- src/components/Overlays/ContextMenu/components/ContextMenuPortal.js
+++ src/components/Overlays/ContextMenu/components/ContextMenuPortal.js
@@ -1,39 +1,55 @@
-import React from 'react';
-import { Portal } from 'react-portal';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Portal } from "react-portal";
+import PropTypes from "prop-types";
 
-import useActivify from '@src/hooks/useActivify';
-import useEscapeKey from '@src/hooks/useEscapeKey';
-import useEnterKey from '@src/hooks/useEnterKey';
-import usePositioner from '../hooks/usePositioner';
+import useActivify from "@src/hooks/useActivify";
+import useEscapeKey from "@src/hooks/useEscapeKey";
+import useEnterKey from "@src/hooks/useEnterKey";
+import usePositioner from "../hooks/usePositioner";
 
-import styles from './ContextMenuPortal.scss';
+import styles from "./ContextMenuPortal.scss";
 
 const ContextMenuPortal = ({
-  active, className, onOverlayClick, children, maxHeight, attachTo, offset,
+  active,
+  className,
+  onClose,
+  children,
+  maxHeight,
+  attachTo,
+  offset,
+  prefixClassName,
 }) => {
   const contextMenuDOM = React.useRef();
   const { enabled, visible } = useActivify(active);
-  const activeClassName = visible ? styles.contextMenuActive : '';
+  const activeClassName = visible ? styles.contextMenuActive : "";
   const position = usePositioner(attachTo, contextMenuDOM, offset);
 
-  useEscapeKey(onOverlayClick);
-  useEnterKey(onOverlayClick);
+  useEscapeKey(onClose);
+  useEnterKey(onClose);
 
   if (!enabled) return null;
   return (
     <Portal>
-      <div className={styles.contextMenuWrapper}>
-        <div className={styles.contextMenuOverlay} onClick={onOverlayClick} />
+      <div
+        data-testid="context-menu"
+        className={`${styles.contextMenuWrapper} ${className} ${prefixClassName}`}
+      >
         <div
-          className={`${activeClassName} ${styles.contextMenu}`}
-          ref={(DOMNode) => {
+          className={`${styles.contextMenuOverlay} ${prefixClassName}-overlay`}
+          onClick={onClose}
+        />
+        <div
+          className={`${activeClassName} ${styles.contextMenu} ${prefixClassName}-context-menu`}
+          ref={DOMNode => {
             if (DOMNode && active) DOMNode.focus();
             contextMenuDOM.current = DOMNode;
           }}
           style={position}
         >
-          <div className={`${styles.contextMenuContent} ${className}`} style={{ maxHeight: maxHeight || '300px' }}>
+          <div
+            className={`${styles.contextMenuContent} ${prefixClassName}-context-menu-content`}
+            style={{ maxHeight: maxHeight || "300px" }}
+          >
             {children}
           </div>
         </div>
@@ -46,9 +62,10 @@ ContextMenuPortal.propTypes = {
   active: PropTypes.bool,
   children: PropTypes.node,
   className: PropTypes.string,
+  prefixClassName: PropTypes.string,
   attachTo: PropTypes.object,
   maxHeight: PropTypes.number,
-  onOverlayClick: PropTypes.func,
+  onClose: PropTypes.func,
   offset: PropTypes.shape({
     left: PropTypes.number,
     top: PropTypes.number,
@@ -60,7 +77,7 @@ ContextMenuPortal.defaultProps = {
     left: 0,
     top: 0,
   },
-  onOverlayClick: () => {},
+  onClose: () => {},
 };
 
 export default ContextMenuPortal;
