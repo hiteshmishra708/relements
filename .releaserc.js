b172848d75e9b2e995933d9b3bef1ca57a800ed1
Pratham Agrawal <pratham.agrawal@haptik.co>
2019-09-03 02:24:46 +0530

    ci(pipeline): release cycle fixes (#36)
    * ci(pipeline): ci pipeline fixes
    
    * ci(pipeline): release cycle fixes
    
    * ci(storybook): next build script

---
+++ .releaserc.js
@@ -0,0 +1,26 @@
+const { GIT_BRANCH: branch } = process.env;
+
+module.exports = {
+  plugins:
+    branch === 'master'
+      ? [
+          '@semantic-release/commit-analyzer',
+          '@semantic-release/release-notes-generator',
+          '@semantic-release/changelog',
+          ['@semantic-release/npm', { npmPublish: false }],
+          [
+            '@semantic-release/git',
+            {
+              assets: ['package.json', 'CHANGELOG.md'],
+              message:
+                'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+          ],
+          '@semantic-release/github',
+        ]
+      : [
+          '@semantic-release/commit-analyzer',
+          ['@semantic-release/npm', { npmPublish: false }],
+        ],
+  release: { branch: branch === 'release' ? 'release' : 'master' },
+};
